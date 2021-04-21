5260552f0491ed2f41ec017f9422a1dcecc949e2
Thijs Boom <twboom@hotmail.nl>
2021-04-21 02:48:10 +0200

    Made basics

---
+++ js/board.js
@@ -0,0 +1,17 @@
+const board = [];
+
+// Creating the board
+
+    // Button
+board.Button = class {
+    constructor() {
+
+    }
+}
+
+    // Board
+board.Board = class {
+    constructor() {
+        
+    }
+}
